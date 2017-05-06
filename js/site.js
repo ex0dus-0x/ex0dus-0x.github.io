176e12dd3d5905391dbfa1a603f558d674e89a5e
ex0dus-0x <ex0dus@codemuch.tech>
2017-05-05 23:01:00 -0400

    Changed blog design

--- js/site.js
+++ js/site.js
@@ -1,11 +1,23 @@
-smoothScroll.init();
-
-
 $( document ).ready(function() {
+  smoothScroll.init();
   $("#rotate").textrotator({
     animation: "flipUp", 
     separator: ",", 
     speed: 2000 
   });
+  
+  $("#quote").html(function() {
+    var quotes = [
+      {text: "/usr/bin"},
+      {text: "cd $GOPATH"},
+      {text: "~/"}
+    ];
+    var quote = quotes[Math.floor(Math.random() * quotes.length)];
+    document.getElementById("quote").innerHTML =
+      '<p class="banner-text text-center">' + quote.text + '</p>' 
+  });
+  
 });
+
+
 
